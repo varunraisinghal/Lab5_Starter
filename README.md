603734db20895788763cecaf9c042c08f9e9335d
varunraisinghal <varun.singhalacademic@yahoo.com>
2022-10-26 22:32:31 -0700

    Finish Lab 5

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 5 - Starter
+Name: Varun Singhal
+Partner Name: Yahui Chen
+
+https://github.com/varunraisinghal/introduction-to-github
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,55 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
-  // TODO
+function init()
+{
+  const jsConfetti = new JSConfetti();
+  const selectHorn = document.getElementById('horn-select');
+  const changeHornImage = document.getElementsByTagName("img")[0];
+  const selectSound = document.querySelector(".hidden");
+  const setVolumeControls = document.getElementById('volume');
+  const setVolumePicture = document.getElementById('volume-controls').getElementsByTagName("img")[0];
+
+  const playSoundButton = document.querySelector('button');
+
+  selectHorn.addEventListener('change', (event) => 
+  {
+    changeHornImage.src = 'assets/images/' + event.target.value + '.svg';
+    selectSound.src = 'assets/audio/' + event.target.value + '.mp3';
+    console.log(selectHorn);
+  });
+
+  playSoundButton.addEventListener('click', (event) => 
+  {
+    selectSound.volume = setVolumeControls.value / 100;
+    selectSound.play();
+    if(changeHornImage.src == 'http://127.0.0.1:5500/assets/images/party-horn.svg')
+    {
+      jsConfetti.addConfetti();
+    }
+ 
+  });
+
+  setVolumeControls.addEventListener('change', (event) => 
+  {
+    setVolumeControls.value = event.target.value;
+    console.log(setVolumeControls.value);
+    if (setVolumeControls.value == 0)
+    {
+      setVolumePicture.src = 'assets/icons/volume-level-0.svg';
+    }
+    else if (setVolumeControls.value < 33)
+    {
+      setVolumePicture.src = 'assets/icons/volume-level-1.svg';
+    }
+    else if (setVolumeControls.value < 67)
+    {
+      setVolumePicture.src = 'assets/icons/volume-level-2.svg';
+    }
+    else
+    {
+      setVolumePicture.src = 'assets/icons/volume-level-3.svg';
+    }
+  });
+  
 }
